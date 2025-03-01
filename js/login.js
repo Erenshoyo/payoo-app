6848977da667802c414aec78f77601d81c8a7b34
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-03-01 22:36:03 +0600

    Done some part

--- js/login.js
+++ js/login.js
@@ -1,18 +1,18 @@
 document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
-    event.preventDefault();//to prevent reload after click.
+    event.preventDefault(); //to prevent reload after click.
     const accountNumber = document.getElementById("account-number").value;
-    if (accountNumber.length == 11) {
-      console.log("Yes");
-    } else {
-      console.log("Enter valid account number");
-    }
     const pin = document.getElementById("pin").value;
-    if (pin.length == 4) {
-      console.log("Yes");
+
+    if (accountNumber === "01937165042") {
+      if (pin === "1234") {
+        console.log("Valid");
+        window.location.href = "./main.html";
+      } else {
+        alert("Enter valid 4-digit PIN");
+      }
     } else {
-      console.log("Enter valid 4-digit PIN");
+      alert("Enter valid account number");
     }
   });
-
